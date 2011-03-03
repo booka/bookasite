5d479402948a8a34afe637477c9cb5ffa09dc5e4
Danigb <danigb@gmail.com>
2011-03-03 17:13:06 +0100

    minor css, better empty content

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,8 +1,13 @@
-module ContentsHelper
+# encoding: utf-8
 
+
+module ContentsHelper
   def render_body(model)
     return if model.blank?
-    if model.content_type == 'text/html'
+    if model.body.blank?
+      title = model.respond_to?(:title) ? model.title :  'esta página'
+      content_tag :div, "<De momento '#{title}' no tiene contenido>", :class => 'body'
+    elsif model.content_type == 'text/html'
       content_tag :div, model.body.html_safe, :class => 'body'
     else
       content_tag :div, model.body, :class => 'body'
