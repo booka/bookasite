d016f9f07c7f8c7cdcff7a62ae953d797028e325
Danigb <danigb@gmail.com>
2011-03-24 13:25:53 +0100

    twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,5 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :twitter, 'cZLzgKHY9phSNDuyFImKTw', 'PV6e164UZaPW9Lq5XHW9C0k37ZZNMxLwv41ewlH8D4'
 end
